4e7e230a13deee373d0af5a61222eaf2efcdad8b
Daniel Baltruschat <danielbaltruschat@outlook.com>
2026-07-04 14:38:48 +0100

    Implement OAM DMA misc functions

--- src/oam_dma.cpp
+++ src/oam_dma.cpp
@@ -1,4 +1,5 @@
 #include "oam_dma.h"
+#include <cstdint>
 
 void OamDma::reset() {
     active = false;
@@ -8,3 +9,15 @@ void OamDma::reset() {
     dot_counter = 0;
     pending_copies = 0;
 }
+
+bool OamDma::is_active() const { return active; }
+
+uint8_t OamDma::read_reg() const {return dma_reg;}
+
+bool OamDma::copy_pending() const {return pending_copies > 0;}
+
+int OamDma::pending_copy_count() const { return pending_copies; }
+
+uint16_t OamDma::source_addr() const { return source_base + index; }
+
+uint16_t OamDma::oam_offset() const { return index; }
