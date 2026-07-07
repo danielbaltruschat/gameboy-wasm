fc6cd323f43e8832cc111ccd9f63e360a1de99f3
Louis Souttar-Stone <LouisSouttarStone@outlook.com>
2026-07-07 20:48:03 +0100

    started writing some CPU tests (none of them pass because i broke my cpu functions)

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,4 @@
 
 
 int main() {
-    CPU cpu;
-    for (int i = 0; i < cpu.testMemory.size(); ++i) {
-        cpu.step();
-    }
-    cpu.debugPrintState();
-    return 0;
 }
--- tests/cputests.cpp
+++ tests/cputests.cpp
@@ -3,6 +3,26 @@
 #include "cpu.h"
 
 
-TEST_CASE("CPU instructions") {
-    CPU cpu;
+TEST_CASE("CPU instruction LB_BC_d16(0x01) runs correctly") {
+    CPU cpu(nullptr, true);
+
+    //loads the next 2 bytes of immediate data into register B and C pair
+    cpu.testMemory = { 0x01, 0x43, 0x42, 0x00, 0x00, 0x00, 0x00, 0x00}; //should load 0x43 to reg C and 0x42 to reg B
+    cpu.step();
+    REQUIRE(cpu.bc() == 0x4342);
+    REQUIRE(cpu.pc == 3);
+}
+
+TEST_CASE("CPU instruction LD_pBC_A(0x02) runs correctly") {
+    CPU cpu(nullptr, true);
+    cpu.a = 0xFF;
+    cpu.b = 0x00;
+    cpu.c = 0x02;
+
+    //stores the content of register A in the memory location of the B and C register pair
+    cpu.testMemory = { 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //should store 0xFF into memory address 2
+    std::vector<uint8_t> answer = {0x02, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    cpu.step();
+    REQUIRE(cpu.testMemory == answer);
+    REQUIRE(cpu.pc == 3);
 }
